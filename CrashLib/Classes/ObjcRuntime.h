5355db3a432ea6bd13d82ea141bc86f6c6f72703
xuteng <xutengc@163.com>
2020-07-07 14:45:12 +0800

    init project

---
+++ CrashLib/Classes/ObjcRuntime.h
@@ -0,0 +1,11 @@
+//
+//  ObjcRuntime.h
+//  ParentalControls_iOS
+//
+//  Created by ap106875 on 2016/12/3.
+//  Copyright © 2016年 studyos. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+void Swizzle(Class c,SEL origSEL, SEL newSEL);
---
+++ CrashLib/Classes/ObjcRuntime.m
@@ -0,0 +1,41 @@
+//
+//  ObjcRuntime.m
+//  ParentalControls_iOS
+//
+//  Created by ap106875 on 2016/12/3.
+//  Copyright © 2016年 studyos. All rights reserved.
+//
+
+#import "ObjcRuntime.h"
+#import <objc/runtime.h>
+
+// 如果是类方法则交换类方法 如果是实例方法 则交换实例方法
+void Swizzle(Class c, SEL origSEL, SEL newSEL) {
+    Method origMethed = class_getInstanceMethod(c, origSEL);
+    Method newMethod = nil;
+    if(!origSEL) {
+        origMethed = class_getClassMethod(c, origSEL);
+        if(!origMethed) {
+            return;
+        }
+        newMethod = class_getClassMethod(c, newSEL);
+        if(!newMethod) {
+            return;
+        }
+    }
+    else {
+        newMethod = class_getInstanceMethod(c, newSEL);
+        if (!newMethod) {
+            return;
+        }
+    }
+    
+    if(class_addMethod(c, origSEL, method_getImplementation(newMethod), method_getTypeEncoding(newMethod))) {
+        class_replaceMethod(c, newSEL, method_getImplementation(origMethed), method_getTypeEncoding(origMethed));
+    }
+    else {
+        method_exchangeImplementations(newMethod, origMethed);
+    }
+}
+
+
